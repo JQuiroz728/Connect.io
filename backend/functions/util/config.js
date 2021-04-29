60cd4a82969da0bb0159134ded460ff79e08ecc2
Jorge Quiroz <gerschjr@gmail.com>
2021-04-29 13:50:39 -0400

    updated frontend and removed security files

--- backend/functions/util/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyBvRcUV8yJhrIxACC99-bxrtpF43DTzzRU",
-    authDomain: "connectio-2df43.firebaseapp.com",
-    projectId: "connectio-2df43",
-    storageBucket: "connectio-2df43.appspot.com",
-    messagingSenderId: "958725951488",
-    appId: "1:958725951488:web:e72f0987bef3111e9c08e5"
-  };
